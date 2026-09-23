55143224fa054037832de3a56083805b44590f01
JJeepSp <jeep14228@gmail.com>
2026-09-23 17:28:54 +0700

    Add files via upload

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,10 @@
-
+f_num = int(input("Enter the first number:\n"))
+S_num = int(input("Enter the second number:\n"))
+mul = f_num * S_num
+print(f_num, "x", S_num, "=", mul)
+if mul > 0 :
+    print("The result is positive.")
+elif mul < 0 :
+    print("The result is negative.")
+else :
+    print("The result is positive and negative.")

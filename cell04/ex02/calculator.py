daa2dcb67ad2a2aa14233bb062032db6327bad27
JJeepSp <jeep14228@gmail.com>
2026-09-24 01:21:15 +0700

    Add files via upload

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,1 +1,7 @@
-
+f_num = int(input("Give me the first number: "))
+s_num = int(input("Give me the second number: "))
+print("Thank you!")
+print(f_num, "+", s_num, "=", f_num + s_num)
+print(f_num, "-", s_num, "=", f_num - s_num)
+print(f_num, "/", s_num, "=", int(f_num / s_num))
+print(f_num, "*", s_num, "=", f_num * s_num)

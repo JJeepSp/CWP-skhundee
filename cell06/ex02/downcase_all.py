f3b6db8aa3f7da21a3a4e27ef32a791d49f80fd1
JJeepSp <jeep14228@gmail.com>
2026-09-25 00:07:09 +0700

    Add files via upload

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,1 +1,11 @@
+import sys
 
+def downcase_it(text) :
+    return text.lower()
+
+if len(sys.argv) == 1 :
+    print("none")
+else :
+    parameters = sys.argv[1:]
+    for para in parameters:
+        print(downcase_it(para))

22aad9b56e5255bec9bd5518ddac77f679c775e3
JJeepSp <jeep14228@gmail.com>
2026-09-25 00:51:57 +0700

    Add files via upload

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,1 +1,17 @@
+import sys
 
+def cut(text) :
+    print(text[:8])
+
+def enlarge(text) :
+    missing = 8 - len(text)
+    print(text + ("Z" * missing))
+
+parameters = sys.argv[1:]
+for word in parameters :
+    if len(word) > 8 :
+        cut(word)
+    elif len(word) < 8 :
+        enlarge(word)
+    else :
+        print(word)

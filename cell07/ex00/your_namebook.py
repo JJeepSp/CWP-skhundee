36f37b133ced616ce62e8447027cab757b356dfb
JJeepSp <jeep14228@gmail.com>
2026-09-25 01:17:22 +0700

    Add files via upload

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,1 +1,16 @@
+def array_of_names(persons_dict):
+    full_names = []
 
+    for first_name, last_name in persons_dict.items():
+        formatted_name = f"{first_name.capitalize()} {last_name.capitalize()}"
+        full_names.append(formatted_name)
+
+    return full_names
+
+persons = {
+"jean": "valjean",
+"grace": "hopper",
+"xavier": "niel",
+"fifi": "brindacier"
+}
+print(array_of_names(persons))

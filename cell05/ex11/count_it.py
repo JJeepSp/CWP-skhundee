1a62d0e3817cfb162893729a06efec75825e3ae8
JJeepSp <jeep14228@gmail.com>
2026-09-24 23:14:13 +0700

    Add files via upload

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,10 @@
+import sys
 
+if len(sys.argv) == 1:
+    print("none")
+else:
+    num_params = len(sys.argv) - 1
+    print(f"parameters: {num_params}")
+    parameters = sys.argv[1:]
+    for word in parameters:
+        print(f"{word}: {len(word)}")

475a4979c19f69694bdccc203eee3c921d7863a7
JJeepSp <jeep14228@gmail.com>
2026-09-24 23:21:56 +0700

    Add files via upload

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,11 @@
+import sys
 
+if len(sys.argv) == 2:
+    text = sys.argv[1]
+    z_count = text.count('z')
+    if z_count > 0:
+        print("z" * z_count)
+    else:
+        print("none")
+else:
+    print("none")

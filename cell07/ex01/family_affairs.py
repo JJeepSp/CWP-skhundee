c84fce82cd879f8702d6625bf970d1982d93504d
JJeepSp <jeep14228@gmail.com>
2026-09-25 01:21:36 +0700

    Add files via upload

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,1 +1,11 @@
+def find_the_redheads(family_dict):
+    return list(filter(lambda key: family_dict[key] == "red", family_dict.keys()))
 
+dupont_family = {
+"florian": "red",
+"marie": "blond",
+"virginie": "brunette",
+"david": "red",
+"franck": "red"
+}
+print(find_the_redheads(dupont_family))

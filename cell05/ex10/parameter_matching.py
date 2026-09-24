0eb758bdd74f730d7dd38da6964fb56f2d2bacd8
JJeepSp <jeep14228@gmail.com>
2026-09-24 23:07:23 +0700

    Add files via upload

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,11 @@
+import sys
 
+if len(sys.argv) == 2 :
+    original = sys.argv[1]
+    user = input("What was the parameter? ")
+    if user == original:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+    print("none")

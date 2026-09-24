836c0b6ef598e5eda8604b56b5c9394c41fc03f9
JJeepSp <jeep14228@gmail.com>
2026-09-24 23:35:45 +0700

    Add files via upload

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,9 @@
+import sys
 
+if len(sys.argv) == 1 :
+    print("none")
+else :
+    para = sys.argv[1:]
+    for word in para :
+        if not word.endswith("ism") :
+            print(f"{word}ism")

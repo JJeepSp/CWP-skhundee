3c7ead617ebee8e8e538f88ddd24f39c480bb80c
JJeepSp <jeep14228@gmail.com>
2026-09-24 23:43:28 +0700

    Add files via upload

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,9 @@
+import sys
 
+if len(sys.argv) == 3 :
+    f_num = int(sys.argv[1])
+    s_num = int(sys.argv[2])
+    num_arr = list(range(f_num, s_num + 1))
+    print(num_arr)
+else :
+    print("none")
